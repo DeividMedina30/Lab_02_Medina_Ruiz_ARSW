d403e0ad8cb59d6faf72eaab1c7e0e641f4a7eca
Cristian Ruiz <cristiansanta13@hotmail.com>
2022-08-22 20:25:22 -0500

    Primera parte parcialmente

--- parte1/src/main/java/edu/eci/arsw/primefinder/Main.java
+++ parte1/src/main/java/edu/eci/arsw/primefinder/Main.java
@@ -3,12 +3,16 @@ package edu.eci.arsw.primefinder;
 public class Main {
 
 	public static void main(String[] args) {
+    int n=30000000;
+    int a1 = n/3 -1;
+    int a2 = n/3 + a1;
 		
-		PrimeFinderThread pft=new PrimeFinderThread(0, 30000000);
+		PrimeFinderThread pft1 = new PrimeFinderThread(0, a1);
+    PrimeFinderThread pft2 = new PrimeFinderThread(a1+1, a2);
+    PrimeFinderThread pft3 = new PrimeFinderThread(a2+1, n);
 		
-		pft.start();
-		
-		
+		pft1.start();
+    pft2.start();
+    pft3.start();
 	}
-	
 }
--- parte1/src/main/java/edu/eci/arsw/primefinder/PrimeFinderThread.java
+++ parte1/src/main/java/edu/eci/arsw/primefinder/PrimeFinderThread.java
@@ -5,8 +5,10 @@ import java.util.List;
 
 public class PrimeFinderThread extends Thread{
 
-	
+  int cont = 5;
+
 	int a,b;
+  boolean running = true;
 	
 	private List<Integer> primes=new LinkedList<Integer>();
 	
@@ -16,31 +18,35 @@ public class PrimeFinderThread extends Thread{
 		this.b = b;
 	}
 
-	public void run(){
-		for (int i=a;i<=b;i++){						
-			if (isPrime(i)){
-				primes.add(i);
-				System.out.println(i);
-			}
-		}
-		
-		
-	}
+  public void run(){
+    while (running){
+      for (int i = a ; i <= b ; i++){						
+        if (isPrime(i) && i != 1){
+          primes.add(i);
+          System.out.println(i);
+        }
+      }
+    }
+  }
 	
 	boolean isPrime(int n) {
-	    if (n%2==0) return false;
-	    for(int i=3;i*i<=n;i+=2) {
-	        if(n%i==0)
-	            return false;
-	    }
-	    return true;
+	  if (n%2==0 && n != 2) return false;
+	  for(int i = 3 ; i*i <= n ; i+=2) {
+	    if(n % i==0)
+	      return false;
+	  }
+	  return true;
 	}
 
 	public List<Integer> getPrimes() {
 		return primes;
 	}
-	
-	
-	
-	
+
+  public void kill(){
+    this.running= false;
+  }
+
+  public void correr(){
+    this.running = true;
+  }
 }
