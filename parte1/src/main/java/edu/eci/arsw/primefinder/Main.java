7d3e72d805ca39cade8a89a34f07239aacde2348
clases clases <clases@labinfo.is.escuelaing.edu.co>
2022-08-23 13:01:10 -0500

    Parte Uno terminada total.

--- parte1/src/main/java/edu/eci/arsw/primefinder/Main.java
+++ parte1/src/main/java/edu/eci/arsw/primefinder/Main.java
@@ -1,18 +1,62 @@
 package edu.eci.arsw.primefinder;
 
+import java.util.Scanner;
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class Main {
 
-	public static void main(String[] args) {
-    int n=30000000;
-    int a1 = n/3 -1;
-    int a2 = n/3 + a1;
-		
-		PrimeFinderThread pft1 = new PrimeFinderThread(0, a1);
-    PrimeFinderThread pft2 = new PrimeFinderThread(a1+1, a2);
-    PrimeFinderThread pft3 = new PrimeFinderThread(a2+1, n);
-		
-		pft1.start();
-    pft2.start();
-    pft3.start();
-	}
+    private static PrimeFinderThread pft1;
+    private static PrimeFinderThread pft2;
+    private static PrimeFinderThread pft3;
+
+    private static void arrancarHilos(int a1, int a2, int n){
+        pft1 = new PrimeFinderThread(0, a1);
+        pft2 = new PrimeFinderThread(a1 + 1, a2);
+        pft3 = new PrimeFinderThread(a2 + 1, n);
+        pft1.start();
+        pft2.start();
+        pft3.start();
+    }
+
+    private static void esperarPulsacionEnterUsuario(){
+        Scanner t = new Scanner(System.in);
+        String enterkey = t.nextLine();
+        while (!enterkey.isEmpty()  ) {
+            esperarPulsacionEnterUsuario();
+        }
+        pft1.renaudarhilo();
+        pft2.renaudarhilo();
+        pft3.renaudarhilo();
+    }
+
+    private static void pausarHilos(){
+        pft1.suspenderhilo();
+        pft2.suspenderhilo();
+        pft3.suspenderhilo();
+        System.out.println("Se pausaron los hilos. Presione enter para reanudar");
+        esperarPulsacionEnterUsuario();
+
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        int n = 30000000;
+        int a1 = n / 3 - 1;
+        int a2 = n / 3 + a1;
+        arrancarHilos(a1,a2, n);
+        Timer temporizador = new Timer();
+        TimerTask tareaEsperar = new TimerTask() {
+            int cont = 0;
+            @Override
+            public void run() {
+                if (cont == 1){
+                    temporizador.cancel();
+                    pausarHilos();
+                }
+                cont ++;
+            }
+        };
+        temporizador.schedule(tareaEsperar,0,5000);
+    }
+
 }
--- parte1/src/main/java/edu/eci/arsw/primefinder/PrimeFinderThread.java
+++ parte1/src/main/java/edu/eci/arsw/primefinder/PrimeFinderThread.java
@@ -4,49 +4,58 @@ import java.util.LinkedList;
 import java.util.List;
 
 public class PrimeFinderThread extends Thread{
-
-  int cont = 5;
-
-	int a,b;
-  boolean running = true;
-	
-	private List<Integer> primes=new LinkedList<Integer>();
-	
-	public PrimeFinderThread(int a, int b) {
-		super();
-		this.a = a;
-		this.b = b;
-	}
-
-  public void run(){
-    while (running){
-      for (int i = a ; i <= b ; i++){						
-        if (isPrime(i) && i != 1){
-          primes.add(i);
-          System.out.println(i);
-        }
-      }
+    private boolean suspender;
+    PrimeFinderThread(){
+        suspender = false;
     }
-  }
-	
-	boolean isPrime(int n) {
-	  if (n%2==0 && n != 2) return false;
-	  for(int i = 3 ; i*i <= n ; i+=2) {
-	    if(n % i==0)
-	      return false;
-	  }
-	  return true;
-	}
 
-	public List<Integer> getPrimes() {
-		return primes;
-	}
+    int a,b;
 
-  public void kill(){
-    this.running= false;
-  }
+    private List<Integer> primes=new LinkedList<Integer>();
 
-  public void correr(){
-    this.running = true;
-  }
+    public PrimeFinderThread(int a, int b) {
+        super();
+        this.a = a;
+        this.b = b;
+    }
+
+    public void run(){
+        for (int i=a;i<=b;i++){
+            if (isPrime(i) && i != 1){
+                primes.add(i);
+                System.out.println(i);
+            }
+            synchronized (this){
+                while(suspender){
+                    try {
+                        wait();
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+            }
+        }
+
+    }
+
+    boolean isPrime(int n) {
+        if (n%2==0 && n != 2) return false;
+        for(int i = 3 ; i*i <= n ; i+=2) {
+            if(n % i==0)
+                return false;
+        }
+        return true;
+    }
+    public List<Integer> getPrimes() {
+        return primes;
+    }
+
+    synchronized void suspenderhilo(){
+        suspender=true;
+    }
+
+    synchronized void renaudarhilo(){
+        suspender=false;
+        notify();
+    }
 }
